496b6ed62d3181fa1dc1dbd10af270abba54eb8c
Ajay Nayak <you@example.com>
2024-10-29 19:26:39 +0530

    changed padding, updated sitemap

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -644,7 +644,7 @@ export const LandingPage = () => {
                             <div className="bg-[#DFDFDF17] w-full flex justify-center items-center rounded-sm text-center">
                                 <h3 className="text-xl md:text-[2.5vw] lg:text-[2.5vw] leading-[1.1] font-bold p-[30px]">Your Unique Perspective Matters to Us.</h3>
                             </div>
-                            <div className="mt-6 w-full p-[30px] bg-[#DFDFDF17] flex flex-col items-center text-lg md:text-[1.1vw] lg:text-[1.1vw] rounded-md">
+                            <div className="mt-6 w-full p-[30px] 2xl:px-[60px] bg-[#DFDFDF17] flex flex-col items-center text-lg md:text-[1.1vw] lg:text-[1.1vw] rounded-md">
                                 <p className="text-center text-base md:text-[1.25vw] lg:text-[1.25vw] font-bold">Take our 8-Question Test and For Your Time, Get...</p>
                                 <ul className="list-disc mt-4">
                                     <li>Access to our GenAI Certification Program designed for every role, domain and functions.</li>
